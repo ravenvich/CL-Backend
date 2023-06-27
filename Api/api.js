2c0fab84eca3901f2311d56d9449962733d32a4d
ravenvich <102375125+ravenvich@users.noreply.github.com>
2023-06-27 18:53:21 +0300

    Update api.js

--- Api/api.js
+++ Api/api.js
@@ -6,4 +6,5 @@ router.post("/users",register);
 router.post("/users/login",login);
 router.put("/users/orders/:email",orders);
 router.get("/users/orders/:email",getOrders);
+router.get("/",(req,res)=>{res.send("Hi")});
 module.exports = router;
